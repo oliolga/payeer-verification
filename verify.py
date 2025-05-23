4fe6b4eb029b4873a47f293fc7c8f660fa2b7d52
oliolga <den-olia@hotmail.it>
2025-05-23 18:51:55 +0200

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,7 +1,6 @@
 def verify(code):
-  if code.startswith("P")
-  and len(code) >= 6:
-          return " Valid Payeer code "
-else:
-return "Invalid code"
+    if code.startswith("P")and len(code) >= 6:
+        return "Valid Payeer code"
+    else:
+        return "Invalid code"
 print (verify("P1131514204"))
